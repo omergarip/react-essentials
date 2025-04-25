bd698e2be2a0ca0acddacc543910ef5bd35f5481
Ömer GARİP <omer.garip@tubitak.gov.tr>
2025-04-25 11:51:33 +0300

    add TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from "react";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
-
-
+import TabButton from "./components/TabButton";
 
 function App() {
   return (
@@ -14,15 +13,19 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-           {
-            CORE_CONCEPTS.map((concept) => (
-              <CoreConcept
-                key={concept.title}
-                {...concept}
-              />
-            ))
-           }
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TabButton(props) {
+  return (
+    <li>
+      <button>{props.children}</button>
+    </li>
+  );
+}
