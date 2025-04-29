7f35364dd2f8fc378f6d1056c7f98caee6b8859f
Ömer GARİP <omer.garip@tubitak.gov.tr>
2025-04-29 18:21:21 +0300

    render content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 // @ts-nocheck
-import React from "react";
-import { CORE_CONCEPTS } from "./data";
+import React, { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
   const handleSelect = (selectedButton) => {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
   };
+
+  let tabContent = <p>Please select a topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -32,6 +47,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
         <h2>Time to get started!</h2>
       </main>
