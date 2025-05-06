f6e52592adee27a786eb0bb0dbd90124480a432f
Ömer GARİP <omer.garip@tubitak.gov.tr>
2025-05-06 18:43:37 +0300

    set component type dynamically

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -29,6 +29,7 @@ const Examples = () => {
   return (
     <Section id="examples" title="Examples">
       <Tabs
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
